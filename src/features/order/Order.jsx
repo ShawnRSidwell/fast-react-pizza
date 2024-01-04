4ca5653770f49b7d11d26d8beff1a661b16db5b6
ShawnRSidwell <shawnrsidwell@gmail.com>
2024-01-04 13:40:03 -0800

    Add style to orderitem

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -7,6 +7,7 @@ import {
   formatCurrency,
   formatDate,
 } from '../../utils/helpers';
+import OrderItem from './OrderItem';
 
 function Order() {
   const order = useLoaderData();
@@ -49,6 +50,11 @@ function Order() {
           (Estimated delivery: {formatDate(estimatedDelivery)})
         </p>
       </div>
+      <ul className="divide-y divide-stone-200 border-b border-t">
+        {cart.map((item) => (
+          <OrderItem item={item} key={item.id} />
+        ))}
+      </ul>
 
       <div className="space-y-2 bg-stone-200 px-6 py-5">
         <p className="text-sm font-medium text-stone-600">
